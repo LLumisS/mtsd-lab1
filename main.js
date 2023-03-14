c97542ed975e74b18362e078f4a6afd433ee955c
LLumisS <vasa16671@gmail.com>
2023-03-14 21:48:30 +0200

    eslint

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 'use strict';
 
-const readlineSync = require("readline-sync");
+const readlineSync = require('readline-sync');
 
 const keys = ['a', 'b', 'c'];
 const values = {
-    a: null,
-    b: null,
-    c: null
+  a: null,
+  b: null,
+  c: null
 };
 
 function question(key) {
-    let value;
+  let value;
 
-    while (true) {
-        value = readlineSync.question(`${key} = `);
+  while (true) {
+    value = readlineSync.question(`${key} = `);
 
-        if (key == 'a' && value == 0)
-            console.log('Error. Value \'a\' cannot be 0');
-        else if (isNaN(value) || (value == ''))
-            console.log('Error. Expected a valid real number');
-        else
-            return value;
-    }
+    if (key === 'a' && value === 0)
+      console.log('Error. Value \'a\' cannot be 0');
+    else if (isNaN(value) || (value === ''))
+      console.log('Error. Expected a valid real number');
+    else
+      return value;
+  }
 }
 
-for (const key of keys)
-{
-    const value = question(key);
-    values[key] = value;
+
+for (const key of keys)     {
+  const value = question(key);
+  values[key] = value;
 }
 
 const a = values['a'];
@@ -38,13 +38,13 @@ console.log(`Equation is: (${a}) x^2 + (${b}) x + (${c}) = 0`);
 
 const D = Math.pow(b, 2) - 4 * a * c;
 
-if(D < 0) {
-    console.log('There are 0 roots.');
+if (D < 0) {
+  console.log('There are 0 roots.');
 } else if (D > 0) {
-    const x1 = (-b + Math.sqrt(D))/(2 * a);
-    const x2 = (-b - Math.sqrt(D))/(2 * a);
-    console.log(`There are 2 roots:\nx1 = ${x1}\nx2 = ${x2}`);
+  const x1 = (-b + Math.sqrt(D)) / (2 * a);
+  const x2 = (-b - Math.sqrt(D)) / (2 * a);
+  console.log(`There are 2 roots:\nx1 = ${x1}\nx2 = ${x2}`);
 } else {
-    const x = (-b)/(2 * a);
-    console.log(`There is 1 root:\nx = ${x}`);
+  const x = (-b) / (2 * a);
+  console.log(`There is 1 root:\nx = ${x}`);
 }
